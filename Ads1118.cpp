077f17c92e8a3e1afae41f13d243e860202e88c8
Colby Rome <cdr013@bucknell.edu>
2015-11-09 19:07:42 -0500

    Commented code nicely

--- Ads1118.cpp
+++ Ads1118.cpp
@@ -2,8 +2,11 @@
    Ads111.cpp - library for interacting with TI ADC
    Created by Colby Rome, October 20, 2015
 */
+
 #include "Arduino.h"
 #include "Ads1118.h"
+
+#define DEBUG
 
 #ifdef DEBUG
     #define DEBUG_PRINT(x)  Serial.println(x)
@@ -13,17 +16,21 @@
 
 Ads1118::Ads1118(int CS_pin)
 {
+    /* Constructor for Ads1118 class.
+     */
     pinMode(CS_pin, OUTPUT);
     _cs = CS_pin;
 }
 
 void Ads1118::begin()
 {
-    //Clock Polarity 0, Clock Phase 1 -> SPI mode 1. MSB first.
-    //Page 25 on datasheet
+    /* Initializes SPI parameters.
+       Clock Polarity 0, Clock Phase 1 -> SPI mode 1. MSB first.
+       Page 25 on datasheet
+     */
     SPI.beginTransaction(SPISettings(4000000, MSBFIRST, SPI_MODE1));
     SPI.begin();
-    digitalWrite(_cs, HIGH); // Do not start transactions yet
+    digitalWrite(_cs, HIGH); // Do not begin transactions yet
     if(self_test()){
         //Error
         DEBUG_PRINT("An error has occurred during self test");
@@ -32,7 +39,9 @@ void Ads1118::begin()
 
 byte Ads1118::self_test()
 {
-    //returns 0 (no error) or 1 (error)
+    /* This self test returns 1 if there is an error communicating with
+       the ADS1118 chip. This may be due to a wiring problem or other.
+     */
     CURRENT_CONFIG = CONFIG_DEFAULT;
     if(CONFIG_DEFAULT != update_config(CONFIG_DEFAULT)){
         return 1;
@@ -42,24 +51,30 @@ byte Ads1118::self_test()
 
 word Ads1118::update_config(word new_config)
 {
-    /* This function executes a 32-bit transaction, returning
-       the configuration register 
+    /* This function executes a 32-bit transaction.
+
+       Input: a word new_config (see datasheet)
+       Output: a word specifying the configuration register. Should be
+       identical to new_config if config updated correctly.
      */
     word readConfig; //this is the return config
     byte MSB, LSB;
     byte MSBConf = ((new_config >> 8) & 0xFF); // bit mask to get MSB
-    byte LSBConf = (new_config & 0xFF);
+    byte LSBConf = (new_config & 0xFF); // bit mask for LSB
+
     digitalWrite(_cs, LOW);
     delayMicroseconds(500);
     // The following 2 transfers result in garbage data:
     SPI.transfer(MSBConf);
     SPI.transfer(LSBConf);
 
+    // We capture the following two transfers to read back the configuration
     MSB = SPI.transfer(MSBConf);
     LSB = SPI.transfer(LSBConf);
     digitalWrite(_cs, HIGH); // Done writing
     readConfig = (MSB << 8) | LSB;
     CURRENT_CONFIG = new_config; // Update global configuration variable
+    delayMicroseconds(1000); // experiment with this
     return readConfig;
 }
 
@@ -68,9 +83,13 @@ word Ads1118::adsReadRaw(word port)
     /* This method executes a 16 bit transaction if the current config
        is already correct, or 2 32-bit transactions otherwise, updating
        the config as necessary.
+
+       Input: A port specifying the pin selection (see header file)
+       Output: Raw requested data from sensor
     */
     word read;
     byte dummy, MSB, LSB;
+
 #ifdef DEBUG
     Serial.print("Current config is: ");
     Serial.println(CURRENT_CONFIG, HEX);
@@ -79,17 +98,20 @@ word Ads1118::adsReadRaw(word port)
     Serial.print("CURRENT_CONFIG & ~PIN_BITMASK = ");
     Serial.println((CURRENT_CONFIG & ~PIN_BITMASK), HEX);
 #endif
+
+    //Only update the configuration *if necessary*
     if((CURRENT_CONFIG & PIN_BITMASK) != port){
-        //Updates the configuration to the proper pin
+        //Updates the configuration to the proper pin:
         update_config((port | (CURRENT_CONFIG & ~(PIN_BITMASK))));
-        delayMicroseconds(1000); // Experiment with this
     }
+
 #ifdef DEBUG
     Serial.print("Input mux: ");
     Serial.println(PIN_BITMASK & CURRENT_CONFIG, HEX);
     Serial.print("MOSI: ");
     Serial.println(CURRENT_CONFIG, HEX);
 #endif
+
     digitalWrite(_cs, LOW);
     delayMicroseconds(500);
     MSB = SPI.transfer((CURRENT_CONFIG >> 8) & 0xFF);
@@ -103,28 +125,44 @@ double Ads1118::convToFloat(word read)
 {
     /* This method returns a double (float) signifying the input signal
        Vin for the proper FS (scaling).
+
+       Input: a binary word from the sensor
+       Output: the floating point representation of the sensor value.
     */
+
+    // See page 26 of datasheet detailing the 6 gain modes.
     float gain[6] = {6.144, 4.096, 2.048, 1.024, 0.512, 0.256};
+
+    // Find the correct gain index using bitshift and bitmask
     float myGain = gain[(((PGA_BITMASK & CURRENT_CONFIG) >> 8) / 2)];
+
 #ifdef DEBUG
     Serial.print("Gain = ");
     Serial.println(gain[(((PGA_BITMASK & CURRENT_CONFIG) >> 8) / 2)], DEC);
 #endif
+
     return (float)((int)read)*myGain/(0x8000);
 }
 
 double Ads1118::adsRead(word port)
 {
-    /* This method returns the floating point representation 
-       */
+    /* This method returns the floating point representation of the desired
+       sensor operation.
+
+       Input: a port selection (see header file)
+       Output: the floating point representation of desired operation.
+     */
     return convToFloat(adsReadRaw(port)); // Reads from port; converts to float
 }
 
 double Ads1118::readTemp()
 {
+    /* This function is still being debug and may or may not work.
+     */
+
     /* This method loads the temperature configuration, and returns the
        floating point representation of the temperature in degrees Celcius.
-    */
+     */
     if(update_config(CONFIG_TEMPERATURE) != CONFIG_TEMPERATURE){
         DEBUG_PRINT("Error: not updating to temp config");
     }
@@ -132,24 +170,25 @@ double Ads1118::readTemp()
     byte MSB, LSB;
     digitalWrite(_cs, LOW);
     delayMicroseconds(500);
+    /*
     MSB = SPI.transfer((CURRENT_CONFIG >> 8) & 0xFF);
     LSB = SPI.transfer(CURRENT_CONFIG & 0xFF);
     digitalWrite(_cs, HIGH);
     delayMicroseconds(500);
     digitalWrite(_cs, LOW);
     delayMicroseconds(500);
+    */
     MSB = SPI.transfer((CURRENT_CONFIG >> 8) & 0xFF);
     LSB = SPI.transfer(CURRENT_CONFIG & 0xFF);
     digitalWrite(_cs, HIGH);
     read = (MSB << 6) | (LSB >> 2);
 #ifdef DEBUG
     Serial.print("Raw MSB =");
-    Serial.println(MSB, HEX);
+    Serial.println(MSB, BIN);
     Serial.print("Raw LSB =");
     Serial.println(LSB, BIN);
     Serial.print("Raw read =");
     Serial.println(read, BIN);
-    Serial.print("0x8000 & read =");
 #endif
     if((0x8000 & read) == 0){
         return (double)read*0.03125; // datasheet page 18
--- Ads1118.h
+++ Ads1118.h
@@ -2,6 +2,7 @@
    Ads1118.h - library for interacting with TI ADC
    Created by Colby Rome, October 19, 2015
 */
+
 #ifndef Ads1118_h
 #define Ads1118_h
 
@@ -12,15 +13,17 @@ class Ads1118
 {
     public:
         Ads1118(int CS_pin);
-        void begin(); //sets up SPI settings
-        word update_config(word new_config); // returns the new config read
-                                             // from the onboard register
-        word adsReadRaw(word port);
-        double readTemp();
-        byte self_test(); // returns 0 (no error), 1 (error)
-        double convToFloat(word read);
+        void begin();
         double adsRead(word port);
-        const word CONFIG_TEMPERATURE = 0x059B;
+        double readTemp();
+        byte self_test();
+
+        word update_config(word new_config);
+        word adsReadRaw(word port);
+        double convToFloat(word read);
+
+        //The following are the defined pins. Voltage is measured from
+        //AINX to GND.
         const word AIN0 = 0x4000;
         const word AIN1 = 0x5000;
         const word AIN2 = 0x6000;
@@ -28,12 +31,16 @@ class Ads1118
 
     private:
         int _cs;          // chip select
-        word CURRENT_CONFIG;
+        word CURRENT_CONFIG; // Global variable holding configuration
 //        const word CONFIG_DEFAULT = 0x048B;
         const word CONFIG_DEFAULT = 0x008B;
+        const word CONFIG_TEMPERATURE = 0x059B;
 
+        // Bit masks
         const word PIN_BITMASK = 0x7000;
         const word PGA_BITMASK = 0x0E00;
+
+        // Gain selections
         const word G_6144 = 0x0000;
         const word G_4096 = 0x0200;
         const word G_2048 = 0x0400;
